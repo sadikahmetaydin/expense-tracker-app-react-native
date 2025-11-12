5ce474c61a91352fbe120989ed153c2e257634ae
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-11-12 20:26:54 +0300

    initialize database and connection postgresql

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -7,10 +8,29 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10,2) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1);
+  }
+}
+
 app.get("/", (req, res) => {
   res.send("Backend is perfectly working!");
 });
 
-app.listen(PORT, () => {
-  console.log("Server is up and running on PORT", PORT);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT", PORT);
+  });
 });
